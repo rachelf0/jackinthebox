e730ee97f537f9ecd2d6bdd2d7ae1682a1260482
Rachel Fritz <rachelfritz0@gmail.com>
2020-08-20 10:25:23 -0500

    create tables

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY,
+    dept_name VARCHAR(30),
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY,
+    role_title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+);
+
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 const mysql = require('mysql2');
+
+async function main() {
+    // get the client
+    const mysql = require('mysql2/promise');
+}
